42ad08fc875d3dfc57e9cd99883e0dc7e05328f3
Neon <neon@neosynth.net>
2022-08-20 17:11:18 -0500

    Created function to clean up metadata vars to prevent data contamination in complex subshell actions

---
+++ tools/functions/clear_metadata.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear_metadata() {
+        metadata=''
+        title=''
+        author=''
+        description=''
+        thumbnail=''
+        published_date=''
+        modified_date=''
+        canonical_url=''
+} 
