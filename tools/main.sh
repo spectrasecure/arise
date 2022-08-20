a59a0d42f60d4558afa7cb665af5968ae8b014be
Neon <neon@neosynth.net>
2022-08-20 18:12:27 -0500

    Added config option for live site URL in main()

--- tools/main.sh
+++ tools/main.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Imports all functions, set configs, and deploys the site
 
+# Set your base website URL here. Do not include a trailing '/'.
+base_url='https://example.site'
 
 # Always run this script with its location as pwd
 cd $(dirname $0)
@@ -13,8 +15,10 @@ source functions/build_toc.sh
 source functions/get_page_metadata.sh
 source functions/clear_metadata.sh
 
-# get_page_metadata ../site/html/fiction/the-dreamer/index.md
+get_page_metadata ../site/html/fiction/the-dreamer/index.md
+echo $relative_url
+echo $canonical_url
 #build_header ../site/html/fiction/the-dreamer/index.md index.html
 
-build_toc $(realpath ../site/html/fiction/shellsite-toc.md)
-ls ../site/html/fiction/
+#build_toc $(realpath ../site/html/fiction/shellsite-toc.md)
+#ls ../site/html/fiction/
