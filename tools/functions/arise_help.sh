73394059cde209704a7fd9a77992dea203f1c7cb
Neon <neon@neosynth.net>
2022-08-22 10:25:20 -0500

    Wrote some glue code to handle cli arguments and do very basic env dependency checks

--- tools/functions/arise_help.sh
+++ tools/functions/arise_help.sh
@@ -2,41 +2,18 @@
 # Prints help
 arise_help() {
 cat <<EOF
-=========================================================
-=========================================================
-
-                                    @@@                   
-           @@#@ @####@@@    ##@  @@@#+@@@@@ @@@@@@@@@@    
-           @++@ @+++++++@   ++@  @+++++++@  @++++++++@    
-          @+++@ @++@@@*++@  ++@ @++++++++@  @+++++++@     
-         @@+*+@ @++@  @@+@  ++@ @++@#+@@@   @@@@@@@@@     
-         @++@+@  @++@  @++@ ++@ @++@#+@                   
-        @++@@+@   @++@ @++@ ++@ @++@#+@@@   @@@@@@@@@     
-       @#+#@@+@   @++@ @++@ ++@  *+++++++@  @+++++++@     
-       @+++@@+@    @++@++@  ++@  @@+++++++  @+++++++@     
-      @++++@@+@ @@@@@++++@  ++@   @@#+@@++@ @@@@@@@@@     
-     @#+@@++@+@ @++++++#@   ++@     #+@@++@               
-     @++@ @+++@ @+@@@@++@   ++@  @@@#+@@++@ @@@@@@@@@@    
-    @++@  @+++@ @+@  @++@   ++@  @++++++++  @++++++++@    
-   @*+@@   @++@ @+@   @++@  ++@  ++++++++@  @++++++++@    
-   @@@@     @@@ @@@    @@@@ @@@ @@@@#+@@@   @@@@@@@@@@    
-                                    @@@                                      
-
-=========================================================
-=========================================================
-
 Welcome to Arise, a static site generator written in Bash
 
 usage: 
         arise build          
                 # Builds the entire site
-        arise [-psr] sourcefile.md
-                # Builds an individual file
+        arise -[p|s|r]
+                # Builds only specific parts of the site
                 # Useful for testing purposes
                 options:
-                        -p: Build a page
-                        -s: Build a sitemap
-                        -r: Build an rss feed
+                        -p: Build pages only mode
+                        -s: Build sitemap only mode
+                        -r: Build rss only mode
 
 Please visit GitHub for more detailed info: 
 https://github.com/neonspectra/arise
--- tools/main.sh
+++ tools/main.sh
@@ -7,12 +7,15 @@ base_url='https://arise.sh'
 # Grabs our current pwd in case we're building an individual file and need to go back to it after loading all of our dependencies
 origin_pwd=$(pwd)
 
-# Always run this script with its location as pwd
-cd $(dirname $0)
+# Always run this script with the repo root location as pwd
+cd '../'"$(dirname $0)"
 
-config=$(realpath ../arise-source/config)
+config=$(realpath arise-source/config)
 
 favicon="/relative/path/to/favicon.ico"
+
+# Check if we're running a current version of bash before potentially causing code that won't run properly on ancient bash versions, like the ones bundled on macOS:
+[ "$BASH_VERSINFO" -lt 5 ] && echo -e 'ERROR: Arise requires Bash version 5 or greater to run. Please install a newer version of Bash or ensure that you are using the newest version installed on your computer.\n\nYour current version of Bash is: '"$BASH_VERSINFO"'\n\nYou can verify the current running version of Bash by running the following command: echo "$BASH_VERSINFO"' && exit 1
 
 # Makes sure that our paths have or don't have a '/' as expected regardless of user input.
 ## Favicon should have a '/' at the start of the path.
@@ -20,17 +23,51 @@ favicon="/relative/path/to/favicon.ico"
 ## Base URL should not have a '/' at the end.
 [[ ${base_url: -1} == '/' ]] && base_url=${base_url::-1}
 
-source functions/arise_help.sh
-source functions/build_header.sh
-source functions/build_footer.sh
-source functions/build_toc.sh
-source functions/build_sitemap.sh
-source functions/build_rss.sh
-source functions/get_page_metadata.sh
-source functions/clear_metadata.sh
+source tools/functions/arise_logo.sh
+source tools/functions/arise_help.sh
+source tools/functions/build_header.sh
+source tools/functions/build_footer.sh
+source tools/functions/build_page.sh
+source tools/functions/build_toc.sh
+source tools/functions/build_sitemap.sh
+source tools/functions/build_rss.sh
+source tools/functions/get_page_metadata.sh
+source tools/functions/clear_metadata.sh
 
 
+arise_logo
+# Basic errors and warnings
+mkdir -p arise-out
+[[ -n "$(ls -A arise-out)" ]] && echo -e 'ERROR: The build output directory "/arise-output" is not empty. Program aborted to prevent overwrite of existing data.\nPlease empty the output directory before running Arise again.' && exit 1
+
+# Read our arguments and set the build mode for processing. Display help if an invalid option is made.
 if [[ $@ == "build" ]]; then
+arise_build="full"
+
+else
+        while getopts ":psr" options; do
+                case "${options}" in
+                        p)
+                                [[ -n "$arise_build" ]] && { arise_help; echo -e '\n\nERROR: Multiple exclusive build options detected. Aborting.'; exit 1; }
+                                arise_build="pages_only";
+                                ;;
+                        s)
+                                [[ -n "$arise_build" ]] && { arise_help; echo -e '\n\nERROR: Multiple exclusive build options detected. Aborting.'; exit 1; }
+                                arise_build="sitemap_only";
+                                ;;
+                        r)
+                                [[ -n "$arise_build" ]] && { arise_help; echo -e '\n\nERROR: Multiple exclusive build options detected. Aborting.'; exit 1; }
+                                arise_build="rss_only";
+                                ;;
+                esac
+        done
+fi
+
+[[ -z "$arise_build" ]] && { arise_help; exit 1; }
+
+echo "$arise_build"
+
+
 #get_page_metadata ../arise-source/fiction/the-dreamer/index.md
 #echo $relative_url
 #echo $canonical_url
@@ -40,32 +77,4 @@ if [[ $@ == "build" ]]; then
 #build_toc $(realpath ../site/html/fiction/shellsite-toc.md)
 #ls ../site/html/fiction/
 
-
-# Rss Testing
-echo "Building site..."
-build_sitemap ../arise-source/sitemap.xml
-
-else
-        while getopts ":p:s:r:" options; do
-                case "${options}" in
-                        p)
-                                echo "building page "" ${OPTARG}"
-                                ;;
-                        s)
-                                echo "building sitemap"" ${OPTARG}"
-                                ;;
-                        r)
-                                echo "building rss feed"" ${OPTARG}"
-                                ;;
-                        :)
-                                echo "command requires arg"
-                                ;;
-                        \?)
-                                echo "Invalid command"
-                                ;;
-                        *)
-                                echo "everything else"
-                                ;;
-                esac
-        done
-fi
+# build_sitemap ../arise-source/sitemap.xml
