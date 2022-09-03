d8d0e9d2c27c49df15b9293a92d21c7578c642ea
Neon <neon@neosynth.net>
2022-09-03 12:16:16 +0100

    Built out page build scripts for building pages.
    Verified Arise doesn't work on macOS because this program relies on GNU
    'cp' and 'date'. HAd no ideas these were dependencies previously. Oh
    well. Need to test if this commit works after I'm back to a GNU machine.

--- tools/functions/subshell/build_page.sh
+++ tools/functions/subshell/build_page.sh
@@ -3,5 +3,24 @@
 # Usage: build_page source.md
 
 build_page() (
-echo "tba lol"
+
+# Switch to page directory
+page=$(basename $1)
+cd $(dirname $1)
+
+get_page_metadata $page
+build_header $page
+
+# Grab everything after the Arise metadata block, run it through pandoc to convert to html, and append to our file in progress
+cat index.md | sed -e '1,/END ARISE/d' | pandoc -f markdown -t html >> index.html
+
+build_footer $page
+
+# Inline Evaluations - DISABLED / BETA
+#while grep "<pre>sh#" index.html
+#do
+#        replacement=$(bash <<< $(sed -n -e s$'\001''<pre>sh#\(.*\)</pre>'$'\001''\1'$'\001''p' < index.html | head -1))
+#        awk 'NR==1,/<pre>sh#.*<\/pre>/{sub(/<pre>sh#.*<\/pre>/, "'"$replacement"'")}{print >"index.html"}' index.html || break
+#done
+
 )
--- tools/functions/subshell/build_page_tree.sh
+++ tools/functions/subshell/build_page_tree.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# TBA - Builds all pages on the site by calling build_page
-# Takes no arguments
+# Builds all pages on the site by calling build_page
+# Root site directory to recursively build from is specified as an argument.
+# Usage: build_page /path/to/arise-source/
 
 build_page_tree() (
-sleep 1
+cd $1
+
+find . -type f -name "*.md" -not \( -path ./config -prune \) | while read fname; do
+build_page $fname
+done
 )
