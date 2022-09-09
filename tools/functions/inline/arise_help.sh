6af5b07973758bfebdbe3027589e2409369c5988
Neon <neon@neosynth.net>
2022-09-09 18:03:08 -0500

    Tweaked help text

--- tools/functions/inline/arise_help.sh
+++ tools/functions/inline/arise_help.sh
@@ -10,22 +10,26 @@
 
 arise_help() {
 cat <<EOF
+Arise, v$arise_version
+
 Welcome to Arise, a static site generator written in Bash
 
-usage: 
-        arise.sh build -[k][f] 
-                # Builds the entire site
-                available in all build modes:
-                        -k: Keeps source files in output
-                        -f: Force overwrite pre-existing output
-        arise.sh -[p|s|r][k][f]
-                # Builds only specific parts of the site
-                # Useful for testing purposes
-                mutually exclusive options:
-                        -p: Build pages only mode
-                        -s: Build sitemap only mode
-                        -r: Build rss only mode
+Usage: 
+------
+arise.sh build -[k][f] 
+        # Builds the entire site
+        available in all build modes:
+                -k: Keeps source files in output
+                -f: Force overwrite pre-existing output
+arise.sh -[p|s|r][k][f]
+        # Builds only specific parts of the site
+        # Useful for testing purposes
+        mutually exclusive options:
+                -p: Build pages only mode
+                -s: Build sitemap only mode
+                -r: Build rss only mode
 
+------
 Please visit GitHub for more detailed info: 
 https://github.com/neonspectra/arise
 
