c0e7179198cb3d2991ac4f8a18435131d48733b8
Neon <neon@neosynth.net>
2022-09-09 16:53:48 -0500

    Added page options for markdown processing and TOC setting.

--- tools/functions/inline/get_page_metadata.sh
+++ tools/functions/inline/get_page_metadata.sh
@@ -2,6 +2,8 @@
 get_page_metadata() {
         if [[ -e $1 ]]; then
                 metadata=$(sed -e '/END ARISE/,$d' < $1)
+
+                # Main page metadata
                 title=$(grep "Title::" <<< $metadata | cut -d '"' -f2)
                 author=$(grep "Author::" <<< $metadata | cut -d '"' -f2)
                 description=$(grep "Description::" <<< $metadata | cut -d '"' -f2)
@@ -9,10 +11,19 @@ get_page_metadata() {
                 thumbnail=$(grep "Thumbnail::" <<< $metadata | cut -d '"' -f2)
                 published_date=$(grep "Published Date::" <<< $metadata | cut -d '"' -f2)
                 modified_date=$(grep "Modified Date::" <<< $metadata | cut -d '"' -f2)
+                
+                # Optional page settings with default settings
+                is_toc="false"
+                process_markdown="true"
+                is_toc=$(grep "toc::" <<< $metadata | cut -d '"' -f2)
+                process_markdown=$(grep "markdown::" <<< $metadata | cut -d '"' -f2)
+                
+                # URL
                 relative_url=$(realpath $(dirname $1) | sed 's@.*arise-out@@g')
                 canonical_url="$base_url""$relative_url"
         else
                 # Clear out metadata so that anything calling this function expecting to get new data cannot get old values on accident if the requested file does not exist.
                 clear_metadata
         fi
+
 } 
--- tools/functions/subshell/build_page.sh
+++ tools/functions/subshell/build_page.sh
@@ -9,12 +9,19 @@ page=$(basename $1)
 cd $(dirname $1)
 
 get_page_metadata $page
-build_header index.html
 
-# Grab everything after the Arise metadata block, run it through pandoc to convert to html, and append to our file in progress
-cat $page | sed -e '1,/END ARISE/d' | pandoc -f markdown -t html >> index.html
-
-build_footer index.html
+if [[ $is_toc == "true" ]]; then
+        build_toc $page
+elif [[ $process_markdown == "false" ]]; then
+        build_header index.html
+        cat $page | sed -e '1,/END ARISE/d' | cat >> index.html
+        build_footer index.html
+else
+        build_header index.html
+        # Grab everything after the Arise metadata block, run it through pandoc to convert to html, and append to our file in progress
+        cat $page | sed -e '1,/END ARISE/d' | pandoc -f markdown -t html >> index.html
+        build_footer index.html
+fi
 
 # Inline Evaluations - DISABLED / BETA
 #while grep "<pre>sh#" index.html
