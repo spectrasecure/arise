99cb5e5e88d5363cea5710dcd21cea4d9f23ace3
Neon <neon@neosynth.net>
2022-09-09 18:24:48 -0500

    Minor fix for typo in inline evaluation

--- functions/subshell/evaluate_inline.sh
+++ functions/subshell/evaluate_inline.sh
@@ -4,7 +4,7 @@
 #############
 # Evaluates inline bash snippets when building pages.
 #
-# This functionality is currently disabled for initial release because even though it "works" when called by build_page the parsing isn't very good, and the syntax for calling an inline evaluation could use some work.
+# This functionality is currently disabled for initial release because even though it "works". When called by build_page, the parsing isn't very good and the syntax for calling an inline evaluation could use some work.
 #
 #############
 # Function Usage: 
@@ -22,4 +22,5 @@ while grep "<pre>sh#" $evaluation_source
 do
         replacement=$(bash <<< $(sed -n -e s$'\001''<pre>sh#\(.*\)</pre>'$'\001''\1'$'\001''p' < $evaluation_source | head -1))
         awk 'NR==1,/<pre>sh#.*<\/pre>/{sub(/<pre>sh#.*<\/pre>/, "'"$replacement"'")}{print >"'"$evaluation_source"'"}' $evaluation_source || break
+done
 )
