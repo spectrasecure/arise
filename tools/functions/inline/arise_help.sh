7665d39ed20cfc874591091eba0017d276bea4cb
Neon <neon@neosynth.net>
2022-09-09 17:57:38 -0500

    Moved files around

--- tools/functions/inline/arise_help.sh
+++ tools/functions/inline/arise_help.sh
@@ -13,12 +13,12 @@ cat <<EOF
 Welcome to Arise, a static site generator written in Bash
 
 usage: 
-        arise build -[k][f] 
+        arise.sh build -[k][f] 
                 # Builds the entire site
                 available in all build modes:
                         -k: Keeps source files in output
                         -f: Force overwrite pre-existing output
-        arise -[p|s|r][k][f]
+        arise.sh -[p|s|r][k][f]
                 # Builds only specific parts of the site
                 # Useful for testing purposes
                 mutually exclusive options:
