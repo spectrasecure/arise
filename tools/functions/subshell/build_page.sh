943c7013681f3de9bf03016c2b0038678fcb6faf
Neon <neon@neosynth.net>
2022-09-09 15:09:17 -0500

    Fixed build_page not outputting to the right file

--- tools/functions/subshell/build_page.sh
+++ tools/functions/subshell/build_page.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Builds a page from a source .md file
+# Builds a page from a source .md file and output the built version to 'index.html' in the same directory
 # Usage: build_page source.md
 
 build_page() (
@@ -9,12 +9,12 @@ page=$(basename $1)
 cd $(dirname $1)
 
 get_page_metadata $page
-build_header $page
+build_header index.html
 
 # Grab everything after the Arise metadata block, run it through pandoc to convert to html, and append to our file in progress
-cat index.md | sed -e '1,/END ARISE/d' | pandoc -f markdown -t html >> index.html
+cat $page | sed -e '1,/END ARISE/d' | pandoc -f markdown -t html >> index.html
 
-build_footer $page
+build_footer index.html
 
 # Inline Evaluations - DISABLED / BETA
 #while grep "<pre>sh#" index.html
