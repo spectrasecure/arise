015d61893652a138a6313abb07011c0108dea78e
Neon <neon@neosynth.net>
2023-11-08 19:18:27 -0600

    typo in escape characters lol

--- lib/functions/inline/clean_xml_string.sh
+++ lib/functions/inline/clean_xml_string.sh
@@ -12,14 +12,14 @@ clean_xml_string() {
 # unclean string -> clean string
         input_string="$1"
         # replace & with &amp;
-        input_string=${input_string//\&/\&38;}
+        input_string=${input_string//\&/\&#38;}
         # replace < with &lt;
-        input_string=${input_string//</\&60;}
+        input_string=${input_string//</\&#60;}
         # replace > with &gt;
-        input_string=${input_string//>/\&62;}
+        input_string=${input_string//>/\&#62;}
         # replace ' with &apos;
-        input_string=${input_string//\'/\&39;}
+        input_string=${input_string//\'/\&#39;}
         # replace " with &quot;
-        input_string=${input_string//\"/\&34;}
+        input_string=${input_string//\"/\&#34;}
         echo "$input_string"
 }
