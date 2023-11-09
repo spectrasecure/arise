9048df91c99896cd47f94ed0970c4f04d687382c
Neon <neon@neosynth.net>
2023-11-08 18:33:53 -0600

    removed escape ampersands since we dont use sed anymore

--- lib/functions/inline/build_header.sh
+++ lib/functions/inline/build_header.sh
@@ -18,34 +18,18 @@ build_header() {
                 # If enabled (default:true), add a configurable content header after the metadata header. The purpose of this is to enable a standardised header for stuff like post dates that should be on *most* pages, but can be disabled on pages the user considers special and wants to build out completely on their own.
                 [[ $content_header == "true" ]] && cat $config/content_header.html >> $1
                 
-                # Replace all tags with their value. Ampersands are a special character in sed, so we have to clean them up using bash string manipulation before running the sed global replace.
+                # Replace all tags in {{this format}} with their value. We do this using Bash pattern replacement.
                 page_contents="$(cat $1)"
-                title=${title//&/\\&}
-                page_contents="${page_contents//\{\{title\}\}/"$title"}"
 
-                author=${author//&/\\&}
+                page_contents="${page_contents//\{\{title\}\}/"$title"}"
                 page_contents="${page_contents//\{\{author\}\}/"$author"}"
-                
-                description=${description//&/\\&}
                 page_contents="${page_contents//\{\{description\}\}/"$description"}"
-                
-                language=${language//&/\\&}
                 page_contents="${page_contents//\{\{language\}\}/"$language"}"
-                
-                thumbnail=${thumbnail//&/\\&}
                 page_contents="${page_contents//\{\{thumbnail\}\}/"$thumbnail"}"
-                
                 page_contents="${page_contents//\{\{published_date\}\}/"$published_date"}"
-                
                 page_contents="${page_contents//\{\{modified_date\}\}/"$modified_date"}"
-                
-                canonical_url=${canonical_url//&/\\&}
                 page_contents="${page_contents//\{\{canonical_url\}\}/"$canonical_url"}"
-                
-                base_url=${base_url//&/\\&}
                 page_contents="${page_contents//\{\{base_url\}\}/"$base_url"}"
-                
-                global_name=${global_name//&/\\&}
                 page_contents="${page_contents//\{\{global_name\}\}/"$global_name"}"
 
                 echo "$page_contents" > $1
